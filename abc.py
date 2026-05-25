9039305d54f998746295a79395cafcfc8aa4c95d
Nytzy1 <bhatianitya0@gmail.com>
2026-05-25 16:03:38 +0000

    coding.py

--- abc.py
+++
@@ -1,3 +0,0 @@
-print("hello")
-a=int(input("enter any no."))
-print(a)
---
+++ begginers coding.py
@@ -0,0 +1,52 @@
+# a=int(input("enter a no."))
+# b=int(input("ente a another a no."))
+# if a>b:
+#     print(f"{a} is the greater no.")
+# else:
+#     print(f"{b} is the greater no.")
+# # this program was about printing a greater no.
+
+# #accept gender and print greeding mssg acc to it
+# a=input("if you are a male then type m or M or if you are a female then type f or F or if you neither then type anything ")
+# if a=="m" or a=="M":
+#     print("good morning sir")
+# elif a=="F" or a=="f":
+#     print("good morning mam")
+# else:
+#     print("none")
+
+#check a no. if it is even or odd
+# a=int(input("enter a no."))
+# if a%2==0:
+#     print("it is an even no.")
+# else:
+#     print("it is an odd no.")
+
+#accept a no. and check if the person is eligible for voting or not ?
+# a=int(input("enter a no."))
+# if a>=18:
+#     print("user is eligible for voting")
+# else:
+#     print("user is not eligible for voting")
+
+# # accept a year and check if it is a leap year or not
+# a=int(input("enter a year"))
+# if a%100==0 and a%4==0:
+#     print("leap year")
+# elif a%4==0:
+#     print("leap year")
+# else:
+#     print("not a leap year")
+
+# #accept in degree celcius and print a description
+# a=int(input("enter a temp in degree celsius"))
+# if a<=0:
+#     print("freezing cold")
+# elif 0<=a<=20:
+#     print("cold")
+# elif 20<a<=30:
+#     print("pleasent weather")
+# else:
+#     print("hot")
+
+

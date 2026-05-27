f30601ba37c12eb878a9c4db7863c056778f3c02
Nytzy1 <bhatianitya0@gmail.com>
2026-05-27 15:44:45 +0000

    todays work done.

---
+++ programsbyuseofwhileloop.py
@@ -0,0 +1,43 @@
+# n=int(input("enter a no."))
+# while n!=0:
+#     print(f"{n%10}")
+#     n=n//10
+
+#this program wa sabout seperating the didgits from the no.
+
+# # accept a no and print its reverse.
+# n=int(input("enter a no."))
+# rev=0
+# while n!=0:
+#     rev=rev*10+n%10
+#     n=(n//10)
+# print(rev)
+
+# # check if a no. is palindrome or not
+# n=int(input("enter a no."))
+# a=n
+# rev=0
+# while n!=0:
+#     rev=rev*10+n%10
+#     n=n//10
+# if rev==a:
+#   print("palindrome no.")
+# else:
+#   print*("not a palindrome no.")
+
+#  #build a  no. guessing game computer picks a random no. and u have to keep guessing until it is correct.
+# import random
+# com=random.randint(1,100)
+# a=0
+# while True:
+#     n=int(input("enter a no. bw 1 and 100"))
+#     a+=1
+
+#     if n==com:
+#         print(f"you have matched it correctly,no of tries.{a}")
+
+#     elif n>com:
+#       print(f"go lower, no of tries.{a}")
+#     else:
+#         print(f"go higher, no. of tries.{a}")
+

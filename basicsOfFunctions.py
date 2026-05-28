a15a6b1a9a6f76cadef9e1f50f50957d30cb1651
Nytzy1 <bhatianitya0@gmail.com>
2026-05-28 15:41:46 +0000

    done little work.

---
+++ basicsOfFunctions.py
@@ -0,0 +1,23 @@
+# # def addition(a,b): # these a and b are parameters.
+# #     print(a+b) 
+# # addition(13,45) #these 13 and 45 are arguments.
+
+# def palindrome_checker(a):
+#     copy=a
+#     rev=0
+#     while a!=0:
+#         rev=rev*10+a%10
+#         a=a//10
+#     if rev==copy:
+#         print("palindrome no.")
+#     else:
+#         print("not a palindrome no.")
+
+# palindrome_checker(111)
+# palindrome_checker(121)
+# palindrome_checker(111)
+# palindrome_checker(121)
+
+
+
+# print("i just said to everone %c" %('5'))
